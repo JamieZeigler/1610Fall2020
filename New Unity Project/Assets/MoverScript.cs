d85ca64b6a9e97aef8ce1a34483071cea3eb431a
Jamie <10839253@my.uvu.edu>
2020-09-02 15:30:33 -0600

    Unity update

---
+++ New Unity Project/Assets/MoverScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverScript : MonoBehaviour
+{
+    public float x, y, z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(message: "Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(x,y,z);
+    }
+}
