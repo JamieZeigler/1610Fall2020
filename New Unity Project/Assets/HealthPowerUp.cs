131073f17e6a636bf0a1c8e53793e11903712934
Jamie <10839253@my.uvu.edu>
2020-09-10 15:40:17 -0600

    Unity Update 4

---
+++ New Unity Project/Assets/HealthPowerUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class HealthPowerUp : MonoBehaviour
+{
+   public int health;
+   public void OnTriggerEnter(Collider other)
+   {
+      health++;
+      print(health);
+   }
+}
